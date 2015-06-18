c9cf910245de25ee34cba26e72df0d0560649d34
perlancar (@pc-office) <perlancar@gmail.com>
2015-06-18 20:58:13 +0700

    End testing of Code::Embeddable

--- lib/Acme/PERLANCAR/Test.pm
+++ lib/Acme/PERLANCAR/Test.pm
@@ -7,10 +7,6 @@ use 5.010001;
 use strict;
 use warnings;
 
-# INSERT_BLOCK: Code::Embeddable import
-
-# INSERT_BLOCK: Code::Embeddable stacktrace_printer
-
 1;
 # ABSTRACT: Distribution to test various things
 
